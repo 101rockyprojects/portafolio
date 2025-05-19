6ef10c6339e3cacf3850ab2b4e853fd95437c199
101rockyprojects <rockybarrios10@gmail.com>
2025-05-18 20:09:54 -0500

    fix recommendations style

--- src/components/views/Recommendations.svelte
+++ src/components/views/Recommendations.svelte
@@ -12,13 +12,13 @@
         {#each $recommendations as rec}
             <div class="reference-card group">
                 {#if rec.comment}
-                    <div class="flex items-center text-center mx-auto pb-2 px-2">
+                    <div class="flex items-center text-center mx-auto px-2">
                         <blockquote class="text-[1.2rem] md:text-xl lg:text-[1.15rem] leading-snug text-white group-hover:text-gold italic">“{rec.comment}”</blockquote>
                     </div>
                 {/if}
                 <div class="flex items-center justify-center text-center gap-4 mx-auto p-2 lg:p-0">
                     <img src={rec.photo} alt={`${rec.person_name} Photo`} class="profile-photo" loading="lazy" />
-                    <div class="flex flex-col justify-center text-start space-y-1 max-w-[55%]">
+                    <div class="flex flex-col justify-center text-start space-y-1 max-w-[58%]">
                         <div class="flex gap-2 items-center max-w-[80%]">
                             <h3 class="text-base lg:text-[0.9rem] font-semibold text-white group-hover:text-gold leading-none text-wrap">{rec.person_name}</h3>
                             <a href={rec.link} target="_blank" rel="noopener noreferrer" aria-label="Photo">
@@ -38,7 +38,7 @@
 
 <style lang="postcss">
     .reference-card {
-        @apply border-white/80 border-[1px] rounded-lg shadow-lg p-2 flex flex-col items-center justify-center text-center w-fit max-w-[45%] lg:w-full lg:min-h-28 lg:max-w-fit hover:bg-ocean hover:border-none hover:scale-105 hover:shadow-xl hover:shadow-gold/20 transition-all duration-200 ease-in-out;
+        @apply border-white/80 border-[1px] rounded-lg shadow-lg p-2 flex flex-col items-center justify-center text-center md:max-w-[45%] lg:w-full lg:min-h-28 lg:max-w-fit hover:bg-ocean hover:border-none hover:scale-105 hover:shadow-xl hover:shadow-gold/20 transition-all duration-200 ease-in-out;
     }
     .profile-photo {
         @apply w-16 md:w-[4.5rem] object-cover rounded-full border-2 border-white/80 group-hover:border-gold;
